e96f4cdbd8489edf47ca024918470c961f8282b9
bigmosi <kinyeramo@gmail.com>
2023-06-10 13:46:47 +0300

    added environment variable and nodemon

--- server.js
+++ server.js
@@ -2,18 +2,21 @@ const express = require('express');
 const debug = require('debug')('server');
 const chalk = require('chalk');
 const morgan = require('morgan');
+const path = require('path');
+
+const PORT = process.env.PORT || 8080;
 
 const app = express();
 
-const port = 8000;
-
 app.use(morgan('tiny'));
+
+app.use(express.static(path.join(__dirname, '/public/')));
 
 app.get('/', (req, res) => {
     res.send('Hello from my app');
 }); 
 
-app.listen(port, () => {
-    debug(`Server is running on port ${chalk.green(port)}`);
+app.listen(PORT, () => {
+    debug(`Server is running on port ${chalk.green(PORT)}`);
 })
 
